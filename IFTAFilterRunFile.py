169efe483fe0319fbc2264f14e7d7d852d46127f
joekern2 <116761823+joekern2@users.noreply.github.com>
2023-03-01 08:46:25 -0600

    Update IFTAFilterRunFile.py

--- IFTAFilterRunFile.py
+++ IFTAFilterRunFile.py
@@ -88,19 +88,19 @@ def run():
         day = tempstringd
 
     daystring = 'Ohalloran_'
-    daystring += str(year)
+    #daystring += str(year)
     
-    #daystring += '2023'
-    
-    daystring += '_'
-    daystring += str(month)
-    
-    #daystring += '01'
+    daystring += '2023'
     
     daystring += '_'
-    daystring += str(day)
+    #daystring += str(month)
     
-    #daystring += '27'
+    daystring += '02'
+    
+    daystring += '_'
+    #daystring += str(day)
+    
+    daystring += '17'
     
     daystring += '.csv'
     
@@ -116,19 +116,19 @@ def run():
         #read xls or xlsx
         fs = gcsfs.GCSFileSystem(project='my-project')
         autodaystring = 'ifta/Ohalloran/'
-        autodaystring += str(year)
+        #autodaystring += str(year)
         
         #autodaystring += '2023'
         
         autodaystring += '_'
-        autodaystring += str(month)
+        #autodaystring += str(month)
         
-        #autodaystring += '01'
+        #autodaystring += '02'
         
         autodaystring += '_'
-        autodaystring += str(day)
+        #autodaystring += str(day)
         
-        #autodaystring += '27'
+        #autodaystring += '17'
         
         autodaystring += '.csv'
         with fs.open(autodaystring) as f:
