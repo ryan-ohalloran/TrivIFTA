a8928ddb5d9429fb08e57fc89e4e1f795d41fc3c
ryan-ohalloran <79421083+ryan-ohalloran@users.noreply.github.com>
2023-12-29 01:21:11 -0600

    removed legacy source code

--- src/events.py
+++
@@ -1,7 +0,0 @@
-
-class NoFuelTaxDataException(Exception):
-    """
-    Raised when no data is returned from the FuelTaxDetails endpoint
-    """
-    def __init__(self, message: str) -> None:
-        super().__init__(message)
