eeeaeb4bfde324c907af95586381221195a34c6b
ryan-ohalloran <79421083+ryan-ohalloran@users.noreply.github.com>
2024-01-10 23:11:00 -0600

    change templates to the correct directory

--- TrivIFTA/TrivIFTA/settings.py
+++ TrivIFTA/TrivIFTA/settings.py
@@ -110,7 +110,7 @@ ROOT_URLCONF = "TrivIFTA.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [BASE_DIR / 'frontend' / 'public'],
+        "DIRS": [os.path.join(BASE_DIR, '..', 'frontend', 'build')],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
