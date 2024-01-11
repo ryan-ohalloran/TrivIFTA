225c4cc7d35ba3ffbeb79e9a9c77638cf3a6d38a
ryan-ohalloran <79421083+ryan-ohalloran@users.noreply.github.com>
2024-01-10 22:29:03 -0600

    wsgi was pointing to the wrong spot again

--- TrivIFTA/TrivIFTA/wsgi.py
+++ TrivIFTA/TrivIFTA/wsgi.py
@@ -11,8 +11,10 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 from whitenoise import WhiteNoise
+from django.conf import settings
+BASE_DIR = settings.BASE_DIR
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "TrivIFTA.settings")
 
 application = get_wsgi_application()
-application = WhiteNoise(application, root=os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), 'frontend', 'build', 'static'))
+application = WhiteNoise(application, root=os.path.join(os.path.dirname(BASE_DIR), 'frontend', 'build', 'static'))
