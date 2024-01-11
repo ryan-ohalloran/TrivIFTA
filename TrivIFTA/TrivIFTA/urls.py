7729db6f24b231573aee1b17c1cbed708f989576
ryan-ohalloran <79421083+ryan-ohalloran@users.noreply.github.com>
2024-01-10 22:03:03 -0600

    change index path

--- TrivIFTA/TrivIFTA/urls.py
+++ TrivIFTA/TrivIFTA/urls.py
@@ -25,5 +25,5 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path('api/run-job/', run_job),
     path('api/entries/<str:date>/', get_entries_by_date),
-    path('', TemplateView.as_view(template_name='index.html')),
+    path('.*', TemplateView.as_view(template_name='index.html')),
 ]
