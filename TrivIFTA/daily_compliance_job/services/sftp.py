9995169379432c1625f5c3ba05ecb7ff9b01621b
ryan-ohalloran <79421083+ryan-ohalloran@users.noreply.github.com>
2024-07-10 13:41:58 -0500

    fixing proxy and connection issues

--- TrivIFTA/daily_compliance_job/services/sftp.py
+++ TrivIFTA/daily_compliance_job/services/sftp.py
@@ -9,7 +9,7 @@ class GeotabSFTP:
         self.host = host
         self.port = port
         self.proxy_url = os.environ.get('QUOTAGUARDSTATIC_URL')
-        
+
         # Parse the proxy URL
         parsed_url = urlparse(self.proxy_url)
         proxy_host = parsed_url.hostname
@@ -17,28 +17,34 @@ class GeotabSFTP:
         proxy_username = parsed_url.username
         proxy_password = parsed_url.password
 
-        # Create the proxy transport
-        self.transport = paramiko.Transport((self.host, self.port))
-        
-        # Set up the proxy
-        self.transport.connect(
-            username=settings.SFTP_USERNAME,
-            password=settings.SFTP_KEY,
-            sock=self._get_proxy_socket(proxy_host, proxy_port, proxy_username, proxy_password)
-        )
-        self.sftp = paramiko.SFTPClient.from_transport(self.transport)
+        print(f"Parsed QuotaGuard URL: host={proxy_host}, port={proxy_port}, username={proxy_username}")
 
-    def _get_proxy_socket(self, proxy_host, proxy_port, proxy_username, proxy_password):
-        sock = socks.socksocket()
-        sock.set_proxy(
+        # Set up the SOCKS5 proxy
+        self.sock = socks.socksocket()
+        self.sock.set_proxy(
             proxy_type=socks.SOCKS5,
             addr=proxy_host,
             port=proxy_port,
             username=proxy_username,
             password=proxy_password
         )
-        sock.connect((self.host, self.port))
-        return sock
+
+        # Connect to the proxy server
+        print(f"Connecting to proxy server {proxy_host}:{proxy_port}")
+        self.sock.connect((self.host, self.port))
+        print(f"Connected to proxy server {proxy_host}:{proxy_port}")
+
+        # Create a paramiko transport object using the proxy-connected socket
+        self.transport = paramiko.Transport(self.sock)
+
+        # Connect to the SFTP server
+        print(f"Connecting to SFTP server {self.host}:{self.port}")
+        self.transport.connect(username=settings.SFTP_USERNAME, password=settings.SFTP_KEY)
+        print(f"Connected to SFTP server {self.host}:{self.port}")
+
+        # Open an SFTP session
+        self.sftp = paramiko.SFTPClient.from_transport(self.transport)
+        print("Opened SFTP session")
 
     def send_to_sftp(self, csv_data: str, filename: str) -> None:
         '''
@@ -49,7 +55,8 @@ class GeotabSFTP:
                 file.write(csv_data)
         except Exception as e:
             # if unsuccessful, show an error message
-            raise Exception(f'Failed to send {filename} to SFTP server.\n\t{e}')
+            print(f'Failed to send {filename} to SFTP server.\n\t{e}')
+            raise
         finally:
             self.sftp.close()
             self.transport.close()
